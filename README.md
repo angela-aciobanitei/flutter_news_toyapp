f26a0bb05907afd64717e2b229a6214cb1d2c882
Angela Aciobanitei <aciobanitei.angela@gmail.com>
2021-02-13 15:52:05 +0000

    Add a basic Flutter widget test

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # A Flutter News App
 
 - [The Boring Flutter Development Show](https://youtube.com/playlist?list=PLjxrf2q8roU3ahJVrSgAnPjzkpGmL9Czl)
+- [Flutter for Android Developers](https://flutter.dev/docs/get-started/flutter-for/android-devs)
 - [Flutter Cookbook](https://flutter.dev/docs/cookbook)
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,15 +1,19 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:flutter_news_toyapp/main.dart';
 
 void main() {
+  testWidgets('a very simple widget test', (WidgetTester tester) async {
+    // Build the app and trigger a frame
+    await tester.pumpWidget(NewsApp());
 
+    // When article tile is not expanded, the launch icon is hidden
+    expect(find.byIcon(Icons.launch), findsNothing);
+
+    // After a article tile tap, the launch icon should be visible
+    await tester.tap(find.byType(ExpansionTile).first);
+    await tester.pump();
+
+    expect(find.byIcon(Icons.launch), findsOneWidget);
+  });
 }
