5b0069b8d76870254d8488eb94df141a4ee3e223
Angela Aciobanitei <aciobanitei.angela@gmail.com>
2021-02-14 15:39:15 +0000

    Add tiny fade transition

--- lib/loading_info.dart
+++ lib/loading_info.dart
@@ -1,23 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-// ignore: must_be_immutable
-class LoadingInfo extends StatelessWidget {
+class LoadingInfo extends StatefulWidget {
   Stream<bool> _isLoading;
 
   LoadingInfo(this._isLoading);
 
+  createState() => LoadingInfoState();
+}
+
+class LoadingInfoState extends State<LoadingInfo>
+    with TickerProviderStateMixin {
+  AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller =
+        AnimationController(vsync: this, duration: Duration(seconds: 1));
+  }
+
   @override
   Widget build(BuildContext context) {
     return StreamBuilder(
-        stream: _isLoading,
+        stream: widget._isLoading,
         builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
-          debugPrint("asd isLoading=${snapshot.data}");
           if (snapshot.hasData && snapshot.data) {
-            return Icon(
-              FontAwesomeIcons.hackerNews,
-              color: Colors.white,
-            );
+            _controller.forward().then((value) => _controller.reverse());
+            return FadeTransition(
+                child: Icon(FontAwesomeIcons.hackerNewsSquare),
+                opacity: _controller);
           }
           return Container();
         });
